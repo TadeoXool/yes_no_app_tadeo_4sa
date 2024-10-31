07234c2af03bc0aa0cfad8ea2c67ff4c97f60efc
Tadeo <tadeo0448@outlook.com>
2024-10-31 11:02:20 -0600

    Yes_no_AnswerFileaddes

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color customColor = Color(0xFF5C11D4);
+
+const List<Color> colorThemes = [];
+
+class AppTheme {
+  ThemeData theme() {
+    return ThemeData(useMaterial3: true);
+  }
+}
---
+++ lib/config/theme/helpers/get_yes_no_answer.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:proyectoapi/damin/entities/message.dart';
+
+class GetYesNoAnswer {
+//se crea intanscia de la clase Dio
+//Para manejar las peticiones HTTP
+  final _dio = Dio();
+}
+
+//obtener la respuesta
+Future<Message> getAnswer() async {
+  //Almacenar la petición GET en una variable
+  final response = await _dio.get('https://yesno:wtf/api');
+
+  //Generar
+  throw UnimplementedError();
+}
